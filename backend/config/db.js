7872aa1c4c0d4429dc39d79b2cdf70ab5b763885
Krishna_Sharma <krishnasharma102938@gmail.com>
2025-10-18 21:03:11 +0530

    'app.jsx'

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,5 +2,5 @@ import mongoose from "mongoose";
 
 export const connectDB = async() => {
     await mongoose.connect('mongodb+srv://organizr1029_db_user:organizr1029@cluster0.ryvvf8x.mongodb.net/')
-        .then (() => console.log('DB connected'));
+        console.log('DB connected')
 }
